288ab241acb3bd17babc147e14c39ddea8a9bc1a
youseon <thesun6201@gmail.com>
2024-05-30 02:24:53 +0900

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -2,59 +2,86 @@ import { useState } from "react";
 import styled from "styled-components";
 
 //0. 스타일 컴포넌트
-const BtnStyled = styled.button`
-  width: 100px;
-  height: 30px;
-  background-color: #222222;
-  color: #eee;
-  border-radius: 8px;
-`;
+//const BtnStyled = styled.button`
+//  width: 100px;
+//  height: 30px;
+//  background-color: #222222;
+//  color: #eee;
+//  border-radius: 8px;
+//`;
 
-const BtnStyled2 = styled.button`
-  width: 50px;
-  height: 30px;
-  background-color: #eee;
-  color: #222;
-  border-radius: 8px;
-  border: none;
-`;
-
-//1.useState 선언
+//1.useState
 function App() {
-	const [count, setCount] = useState(0);
+	const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "잠자기",
+      completed: true,
+    },
+    {
+      id:2,
+      test: "일찍일어나기",
+      completed: false,
+    },
+  ]);
 	
-//2.카운트 함수 만들기
-const addCount = () => {
-  setCount(count + 1);
+//2.state 변경 함수
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+//제출하기
+const handleAddTodo = (event) => {
+  event.preventDefault();
+  if(!input.trim()) return;
+  const newTodo = {
+    id: Date.now(),
+    text: input,
+    completed: false,
+  };
+  setTodos([...todos, newTodo]);
+  setInput("");
 };
 
-const minusCount = () => {
-  setCount(count - 1);
+//삭제하기 버튼 함수
+const handleDeleteTodo = (id) => {
+  setTodos(todos.filter((todo) => todo.id !== id));
 };
 
-const resetCount = () => {
-  setCount(0)
+//수정하기 함수
+const handleToggleTodo = (id) => {
+  setTodos(
+    todos.map((todo)=>
+      todo.id === id ? {...todo, completed: !todo.completed} : todo
+    )
+  );
 };
 
-//3. 카운트 함수적용
   return (
     <div>
-      <div style={{
-        width: "300px", 
-        margin: "40px auto",
-        fontSize: "24px",
-        fontWeight: "bold",
-        textAlign: "center"
-        }} >{count}</div>
-      <div style={{
-        width: "300px", 
-        height:"50px",
-        margin: "0 auto"
-        }}>
-      <BtnStyled onClick={addCount}>1더하기</BtnStyled>
-      <BtnStyled style={{marginLeft: "4px"}} onClick={minusCount}>1빼기</BtnStyled>
-      <BtnStyled2 style={{marginLeft: "4px"}} onClick={resetCount}>리셋</BtnStyled2>
-      </div>
+      <h1>할 일 목록</h1>
+      <form onSubmit={handleAddTodo}>
+        <input type="text" placeholder="할 일을 추가하세요" 
+        value={input}
+        onChange={handleInputChange}
+        />
+        <button type="submit">추가</button>
+      </form>
+      <ul>
+        {todos.map((todo) =>(
+          <li
+          key={todo.id}
+          style={{textDecoration: todo.completed ? "line-through":"none"}}
+          >
+            {todo.text}
+            <button onClick={() => handleToggleTodo(todo.id)}>
+              {todo.completed ? "취소": "완료"}
+              </button>
+            <button onClick={() => handleDeleteTodo(todo.id)}>삭제</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
